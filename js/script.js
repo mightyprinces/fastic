5e9003089399e49eefa36fce7daa77b1a25b21f9
Mightyprincess <happyrabbit62@gmail.com>
2023-05-27 20:56:39 +0700

    feat: add fast plan pages and styles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
-const buttonEndStart = document.querySelector('.button--start-end');
-buttonEndStart.addEventListener('click', () => {
-    console.log('gggg');
-})
+const buttonUp = document.querySelector(".btn-up");
+const buttonDown = document.querySelector(".btn-down");
+const weekdayProgress = document.querySelector(
+  ".weekday-item__progress--actual"
+);
+const weekdayItem = document.querySelector(".weekday-item");
+
+let counterTop = 100;
+let weekdayProgressTop = 0;
+weekdayProgress.style.top = counterTop.toString() + "%";
+
+buttonUp.addEventListener("click", () => {
+  if (counterTop > 0 && counterTop <= 100) {
+    counterTop -= 10;
+    weekdayProgress.style.top = counterTop.toString() + "%";
+    console.log("top", weekdayProgress.style.top);
+
+    isWeekdayFastingDone(counterTop);
+  }
+});
+
+buttonDown.addEventListener("click", () => {
+  if (counterTop >= 0 && counterTop < 100) {
+    counterTop += 10;
+    weekdayProgress.style.top = counterTop.toString() + "%";
+    console.log("top", weekdayProgress.style.top);
+
+    isWeekdayFastingDone(counterTop);
+  }
+});
+
+function isWeekdayFastingDone(counterTop) {
+  console.log("in isWeekdayFastingDone");
+  if (counterTop === 0) {
+    weekdayItem.classList.add("weekday-item--done");
+  } else {
+    weekdayItem.classList.remove("weekday-item--done");
+  }
+}
